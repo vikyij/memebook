f496ff8ecf30a12d414a58b52d06f37f3af00e94
vikyij <vikkyij54@gmail.com>
2021-06-13 22:54:37 +0100

    download depencies and create signup page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
+import Signup from './pages/signup'
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <Signup />         
   );
 }
 
---
+++ src/pages/signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react' 
+import styles from './signup.module.css'
+
+const Signup = () => {
+    return (
+        <div className={styles.bg}>
+        Signup
+        </div>
+    )
+}
+
+export default Signup
